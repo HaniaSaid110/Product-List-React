5b83733c1deb65864047688a0f7251b98ffa3e39
Hania Said <haniasaid110@gmail.com>
2026-04-01 20:57:27 +0200

    feat: add product data and configure deployment settings for GitHub Pages

--- src/js data/data.js
+++ src/js data/data.js
@@ -1,8 +1,13 @@
+import cat1 from '../assets/images/cat.jpg';
+import cat2 from '../assets/images/cat2.jpg';
+import cat3 from '../assets/images/cat3.jpg';
+import cat4 from '../assets/images/cat4.jpg';
+
 export default [
-  { id: 1, title: `prod 1`, price: 10, img: `src/assets/images/cat.jpg` },
-  { id: 2, title: `prod 2`, price: 20, img: `src/assets/images/cat2.jpg` },
-  { id: 3, title: `prod 3`, price: 30, img: `src/assets/images/cat3.jpg` },
-  { id: 4, title: `prod 4`, price: 40, img: `src/assets/images/cat4.jpg` },
-  { id: 5, title: `prod 5`, price: 50, img: `src/assets/images/cat4.jpg` },
-  { id: 6, title: `prod 6`, price: 60, img: `src/assets/images/cat4.jpg` },
+  { id: 1, title: `prod 1`, price: 10, img: cat1 },
+  { id: 2, title: `prod 2`, price: 20, img: cat2 },
+  { id: 3, title: `prod 3`, price: 30, img: cat3 },
+  { id: 4, title: `prod 4`, price: 40, img: cat4 },
+  { id: 5, title: `prod 5`, price: 50, img: cat4 },
+  { id: 6, title: `prod 6`, price: 60, img: cat4 },
 ];
