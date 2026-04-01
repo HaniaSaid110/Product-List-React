e3b1121e5c4cd1ec254189c58c638e18fd416587
Hania Said <haniasaid110@gmail.com>
2026-04-01 21:07:30 +0200

    feat: redeployed

--- src/js data/data.js
+++ src/js data/data.js
@@ -1,7 +1,7 @@
-import cat1 from '../assets/images/cat.jpg';
-import cat2 from '../assets/images/cat2.jpg';
-import cat3 from '../assets/images/cat3.jpg';
-import cat4 from '../assets/images/cat4.jpg';
+import cat1 from "../assets/images/cat.jpg";
+import cat2 from "../assets/images/cat2.jpg";
+import cat3 from "../assets/images/cat3.jpg";
+import cat4 from "../assets/images/cat4.jpg";
 
 export default [
   { id: 1, title: `prod 1`, price: 10, img: cat1 },
